cf2a426de23e0431f4dbe9f01769d3decd26c16c
Tom Martin <tom.martin1239@gmail.com>
2022-12-13 16:22:58 +0000

    Fix all default clippy warnings

--- src/util.rs
+++ src/util.rs
@@ -1,17 +1,13 @@
-use clap::Parser;
 use std::num::ParseIntError;
 
 use crate::util::DuplicateBehaviour::{TakeCurrent, TakeUntracked};
 
-pub const REGION_DIAMETER_IN_CHUNKS: u32 = 32;
-pub const CHUNKS_PER_REGION: u32 = REGION_DIAMETER_IN_CHUNKS * REGION_DIAMETER_IN_CHUNKS;
 pub const SECTOR_SIZE: usize = 4096;
-pub const SECTOR_SIZE_BITS: u32 = 12;
 
 pub const SIZE_BITS: u32 = 8;
 pub const SIZE_MASK: u32 = (1 << SIZE_BITS) - 1;
 
-#[derive(Clone, Copy, PartialEq, Debug, clap::ValueEnum)]
+#[derive(Clone, Copy, Eq, PartialEq, Debug, clap::ValueEnum)]
 pub enum DuplicateBehaviour {
     TakeCurrent,   // take the chunk referenced by the header file
     TakeUntracked, // take the chunk not referenced by the header file, choose if there are several
@@ -28,17 +24,17 @@ pub fn chunk_position_from_entry_idx(region_position: (i32, i32), entry_idx: u16
     let entry_x = entry_idx & 0x1f;
     let entry_z = entry_idx >> 5;
 
-    return (
+    (
         (region_position.0 << 5) + (entry_x as i32),
         (region_position.1 << 5) + (entry_z as i32),
-    );
+    )
 }
 
 pub fn set_header_entry(bytes: &mut [u8], header_offset: usize, sector_idx: usize, size: u8) {
     bytes[header_offset + 3] = size;
-    bytes[header_offset + 2] = ((sector_idx >> 0) & 0xff) as u8;
+    bytes[header_offset + 2] = ((sector_idx) & 0xff) as u8;
     bytes[header_offset + 1] = ((sector_idx >> 8) & 0xff) as u8;
-    bytes[header_offset + 0] = ((sector_idx >> 16) & 0xff) as u8;
+    bytes[header_offset] = ((sector_idx >> 16) & 0xff) as u8;
 
     let packed = read_bigendian_u32(bytes, header_offset);
     let written_offset = packed >> SIZE_BITS;
@@ -52,7 +48,7 @@ pub fn read_bigendian_u32(bytes: &[u8], header_offset: usize) -> u32 {
     (bytes[header_offset + 3] as u32)
         | (bytes[header_offset + 2] as u32) << 8
         | (bytes[header_offset + 1] as u32) << 16
-        | (bytes[header_offset + 0] as u32) << 24
+        | (bytes[header_offset] as u32) << 24
 }
 
 pub fn trim_newline(s: &mut String) {
